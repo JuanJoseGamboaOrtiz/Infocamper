e24af32d07cdb205369fe59d5107de73776795cd
Juan Jose Gamboa <juangamboaortiz@gmail.com>
2023-06-09 14:34:47 -0500

    feat: :sparkles: Función que se encarga de guardar mejorada
    En la linea 27 y 28 se añade la funcionalidad de recargar la pagina para evitar posibles duplicados al momento de agregar

--- index.php
+++ index.php
@@ -1,22 +1,5 @@
 <?php
-        session_destroy();
-        $config = [
-            'http' => [
-                'method' => 'GET'
-        ]
-        ];
-        
-        $stream= stream_context_create($config);
-
-        $campers = file_get_contents('https://6480f4b2f061e6ec4d4a1e27.mockapi.io/campers', false, $stream);
-        $campers = json_decode($campers,true);
-
-        function subirDatos(string $info, array $campers1) {
-            if( isset($_GET['existe'])){
-                $text=$campers1[$_GET['existe']-1][$info];
-                echo $text;
-            }
-        }
+        include('traercampers.php');
 
         if (isset($_POST['agregar'])){
             $data=[
@@ -41,6 +24,8 @@
 
             $config= stream_context_create($config);
             $_DATA= file_get_contents('https://6480f4b2f061e6ec4d4a1e27.mockapi.io/campers',false,$config);
+            header("Location: " . $_SERVER['REQUEST_URI']);
+            exit;
         }
 
 ?>
@@ -99,7 +84,6 @@
             <th>Acción</th>
         </tr>
         <?php
-    
             if (isset($campers)) {
                 foreach ($campers as $camper) {
                 echo '
@@ -116,7 +100,6 @@
                         </tr>';
                 }
             }
-
         ?>
     </table>
         
